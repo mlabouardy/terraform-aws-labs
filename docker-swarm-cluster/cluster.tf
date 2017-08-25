de4794dcc0ae59b56f989a73b82a23789e1e3efc
mlabouardy <mohamed@labouardy.com>
2017-08-25 18:52:40 +0200

    associate security group to instance

--- docker-swarm-cluster/cluster.tf
+++ docker-swarm-cluster/cluster.tf
@@ -3,11 +3,40 @@ resource "aws_key_pair" "default"{
   public_key = "${file("${var.key_path}")}"
 }
 
+resource "aws_security_group" "default" {
+  name = "sgswarmcluster"
+
+  # Allow SSH
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Expose HTTP
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Enable ICMP
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "master" {
   ami = "${var.ami}"
   instance_type = "${var.instance_type}"
   key_name = "${aws_key_pair.default.id}"
   user_data = "${file("${var.bootstrap_path}")}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
 
   tags {
     Name  = "master"
