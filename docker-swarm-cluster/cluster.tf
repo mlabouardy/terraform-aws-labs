ea78dbfb744205a3218ffc22bc9d8d40e70e612d
mlabouardy <mohamed@labouardy.com>
2017-08-25 20:30:14 +0200

    allow outgoing traffic

--- docker-swarm-cluster/cluster.tf
+++ docker-swarm-cluster/cluster.tf
@@ -14,10 +14,32 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Expose HTTP
+  # Allow HTTP
   ingress {
     from_port = 80
     to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTPS
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port = 443
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
--- docker-swarm-cluster/install-docker.sh
+++ docker-swarm-cluster/install-docker.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-echo "hello world" > /home/ec2-user/test
+yum update
+yum install -y docker
+service docker start
+usermod -aG docker ec2-user
