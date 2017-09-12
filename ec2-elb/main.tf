f905651602d9a3093139cb138ae639c985cd291b
mlabouardy <mohamed@labouardy.com>
2017-09-12 15:03:01 +0200

    setup load balancer

---
+++ ec2-elb/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+resource "aws_security_group" "default" {
+  name = "ec2-elb-sg"
+
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "default" {
+  key_name = "ec2-elb-key"
+  public_key = "${file("${var.key_path}")}"
+}
+
+resource "aws_instance" "default" {
+  count = 2
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name = "${aws_key_pair.default.id}"
+  security_groups = ["${aws_security_group.default.name}"]
+  user_data = "${file("boostrap-server-${count.index}.sh")}"
+
+  tags {
+    Name = "server${count.index}"
+  }
+}
