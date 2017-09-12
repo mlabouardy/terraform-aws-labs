e33ae27c323bcab7be6968695b7fbac612a05f71
mlabouardy <mohamed@labouardy.com>
2017-09-12 19:42:50 +0200

    fix elb instances

--- ec2-elb/main.tf
+++ ec2-elb/main.tf
@@ -32,15 +32,51 @@ resource "aws_key_pair" "default" {
   public_key = "${file("${var.key_path}")}"
 }
 
-resource "aws_instance" "default" {
-  count = 2
+resource "aws_instance" "server1" {
   ami = "${var.ami}"
   instance_type = "${var.instance_type}"
   key_name = "${aws_key_pair.default.id}"
   security_groups = ["${aws_security_group.default.name}"]
-  user_data = "${file("bootstrap-server${count.index+1}.sh")}"
+  user_data = "${file("bootstrap-server1.sh")}"
 
   tags {
-    Name = "server${count.index+1}"
+    Name = "server1"
+  }
+}
+
+resource "aws_instance" "server2" {
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name = "${aws_key_pair.default.id}"
+  security_groups = ["${aws_security_group.default.name}"]
+  user_data = "${file("bootstrap-server2.sh")}"
+
+  tags {
+    Name = "server2"
+  }
+}
+
+resource "aws_elb" "default" {
+  name = "ec2-elb"
+  instances = ["${aws_instance.server1.id}", "${aws_instance.server2.id}"]
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "tcp"
+    lb_port = 80
+    lb_protocol = "tcp"
+  }
+
+  health_check {
+    target = "HTTP:80/"
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 30
+    timeout = 5
+  }
+
+  tags {
+    Name = "ec2-elb"
   }
 }
